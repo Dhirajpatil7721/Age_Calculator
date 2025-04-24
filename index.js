8c33a614721e5ef14c33c2236bb65eb14bb3eabe
Dhiraj Patil <151619876+Dhirajpatil7721@users.noreply.github.com>
2025-04-24 16:11:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,46 +3,40 @@ userinput.max = new Date().toISOString().split("T")[0];
 
 let result = document.getElementById("p");
 
+function getDaysInMonth(year, month) {
+  return new Date(year, month + 1, 0).getDate();
+}
+
 function CalculateAge() {
-    let birthdate = new Date(userinput.value)
+  if (!userinput.value) {
+    result.innerHTML = `<span>Please enter your birthdate!</span>`;
+    return;
+  }
 
-    let d1 = birthdate.getDate()
-    let m1 = birthdate.getMonth() + 1
-    let y1 = birthdate.getFullYear()
+  let birthdate = new Date(userinput.value);
+  let today = new Date();
 
-    let today = new Date();
+  let d1 = birthdate.getDate();
+  let m1 = birthdate.getMonth();
+  let y1 = birthdate.getFullYear();
 
-    let d2 = today.getDate()
-    let m2 = today.getMonth() + 1
-    let y2 = today.getFullYear()
+  let d2 = today.getDate();
+  let m2 = today.getMonth();
+  let y2 = today.getFullYear();
 
+  let y3 = y2 - y1;
+  let m3 = m2 - m1;
+  let d3 = d2 - d1;
 
-    let d3, m3, y3;
+  if (d3 < 0) {
+    m3--;
+    d3 += getDaysInMonth(y2, m2 - 1);
+  }
 
-    y3 = y2 - y1;
+  if (m3 < 0) {
+    y3--;
+    m3 += 12;
+  }
 
-    if (m2 >= m1) {
-        m3 = m2 - m1;
-    } else {
-        y3--;
-        m3 = 12 + m2 - m1;
-    }
-    if (d2 >= d1) {
-        d3 = d2 - d1;
-    } else {
-        m3--;
-        d3 = getDaysInMonth(y1, m1) + d2 - d1;
-    }
-    if (m3 < 0) {
-        m3 = 11;
-        y3--;
-    }
-
-    result.innerHTML = `You are <span>${y3}</span> years, <span>${m3}</span> months, and <span>${d3}</span> days old...!`;
-    console.log(y3, m3, d3);
-
-
-    function getDaysInMonth(year, month) {
-        return new Date(year, month, 0).getDate();
-    }
+  result.innerHTML = `You are <span>${y3}</span> years, <span>${m3}</span> months, and <span>${d3}</span> days old...!`;
 }
